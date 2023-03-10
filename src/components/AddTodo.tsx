cea85e45f535b47c69f0366405806c2bfe9f879a
Ali Jahankah <aral2@yahoo.com>
2023-03-10 15:49:07 +0000

    unit test coverage

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,34 +1,44 @@
-import React, { useState } from 'react'
-
-type Props = { 
-  saveTodo: (e: React.FormEvent, formData: ITodo | any) => void 
-}
-
-const AddTodo: React.FC<Props> = ({ saveTodo }) => {
-  const [formData, setFormData] = useState<ITodo | {}>()
-
+import React, { useState } from "react";
+type Props = {
+  saveTodo: (e: React.FormEvent, formData: ITodo | any) => void;
+};
+const AddTodo = ({ saveTodo }: Props) => {
+  const [formData, setFormData] = useState<ITodo | {}>();
   const handleForm = (e: React.FormEvent<HTMLInputElement>): void => {
     setFormData({
       ...formData,
       [e.currentTarget.id]: e.currentTarget.value,
-    })
-  }
-
+    });
+  };
   return (
-    <form className='Form' onSubmit={(e) => saveTodo(e, formData)}>
+    <form className="Form" onSubmit={(e) => saveTodo(e, formData)}>
       <div>
         <div>
-          <label htmlFor='name'>Name</label>
-          <input onChange={handleForm} type='text' id='name-todo' />
+          <label htmlFor="name">Name</label>
+          <input
+            onChange={handleForm}
+            type="text"
+            id="name"
+            data-testid="nameInput"
+          />
         </div>
         <div>
-          <label htmlFor='description'>Description</label>
-          <input onChange={handleForm} type='text' id='description' />
+          <label htmlFor="description">Description</label>
+          <input
+            onChange={handleForm}
+            type="text"
+            id="description"
+            data-testid="descriptionInput"
+          />
         </div>
       </div>
-      <button disabled={formData === undefined ? true: false} >Add Todo</button>
+      <button
+        disabled={formData === undefined ? true : false}
+        data-testid="btn"
+      >
+        Add Todo
+      </button>
     </form>
-  )
-}
-
-export default AddTodo
+  );
+};
+export default AddTodo;
