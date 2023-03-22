c030626abd4eed1208745baf038d8a2165d5d7ee
Ali Jahankah <aral2@yahoo.com>
2023-03-22 17:29:29 +0000

    reviewing Back-end using: Node | TS | Mongoose

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Express } from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app: Express = express();
+app.use(cors());
+dotenv.config();
+
+const URI: string = process.env.DATABASE_URI!;
+const PORT = process.env.PORT || 4000;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as any;
+
+mongoose
+  .connect(URI, options)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Api is running on port ${PORT}.`))
+  )
+  .catch((err) => console.log(err));
